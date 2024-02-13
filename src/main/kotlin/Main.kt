b82e6882fbe769a4f05c09adc140b6ef0181901e
akj23 <akj23@ic.ac.uk>
2024-02-13 16:42:45 +0000

    Add Default Files

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,13 @@
+package org.terairk.lox
+
+import java.io.BufferedReader
+import java.io.IOException
+import java.io.InputStreamReader
+import java.nio.charset.Charset
+import java.nio.file.Files
+import java.nio.file.Paths
+import java.util.List
+
+fun main() {
+    println("Hello World!")
+}
